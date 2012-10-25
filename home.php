c027329647eca1564a19e9ea033ea5c30d6e7d33
Kevin Cook <kevinac4@gmail.com>
2012-10-25 02:36:08 -0600

    testing, gone soon

--- home.php
+++ home.php
@@ -50,7 +50,6 @@ $xml = simplexml_load_file('xml/copydeck.xml');
 	<!-- end Adserving Tag-->
         </head>
 	<body class="index">
-<h1>test, sorry</h1>    
     <!-- start IMI -->
 		<script type="text/javascript">
 		document.write(unescape('%3Cscript src="' + document.location.protocol + '//d1ivexoxmp59q7.cloudfront.net/imi/live.js" type="text/javascript"%3E%3C/script%3E'));
