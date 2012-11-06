2d6b2ca9122033f925bdabba42729882b9124811
Kevin Cook <kevinac4@gmail.com>
2012-11-06 08:40:41 -0700

    purging git cache

--- hana.php
+++ hana.php
@@ -250,7 +250,7 @@ foreach ($xml->hana->secondaryPages->page as $page) {
 					<div id="reservation_cta">
 						<p class="reservation_title">1-855-TO-TRAVAASA  (1-855-868-7282)</p>
                                                 <div id="book-buttons">
-                                                <div id="book-hana"><a href="https://gc.synxis.com/rez.aspx?Hotel=26987&Chain=10237&template=HNMHM&shell=HNMHM2&adult=2" target="_blank" onclick="_gaq.push(['_link', 'https://gc.synxis.com/rez.aspx?Hotel=26987&Chain=10237&template=HNMHM&shell=HNMHM2&adult=2']); return false;">Book Hana</a></div>
+                                                <div id="book-hana"><a href="https://gc.synxis.com/rez.aspx?Hotel=26987&Chain=10237&template=HNMHM&shell=HNMHM2" target="_blank" onclick="_gaq.push(['_link', 'https://gc.synxis.com/rez.aspx?Hotel=26987&Chain=10237&template=HNMHM&shell=HNMHM2']); return false;">Book Hana</a></div>
 						<div id="book-austin"><a href="https://gc.synxis.com/rez.aspx?Hotel=28064&Chain=10237&template=AUSTC&shell=AUSTC4" target="_blank" onclick="_gaq.push(['_link', 'https://gc.synxis.com/rez.aspx?Hotel=28064&Chain=10237&template=AUSTC&shell=AUSTC4']); return false;">Book Austin</a></div>
                                                 <div class="clearbox"></div>
                                                 <div id="getonthelist"><a href="http://www.data2gold.com/gallery/travaasa/eClub/eClub.html">Get on the list</a></div>
@@ -292,7 +292,7 @@ foreach ($xml->hana->secondaryPages->page as $page) {
 						
 						
 					
-							<img src="/<?php echo $imgSrc; ?>" />
+							<img src="/<?php echo $imgSrc; ?>" class="mainimg" />
 							<?php if($imageCaption != ""):?>
 							<div id="main_image_caption">
 								<p><?php echo $imageCaption; ?></p>
@@ -348,7 +348,7 @@ foreach ($xml->hana->secondaryPages->page as $page) {
 
 		<?php include("php/footer_social.include.php"); ?>
         
-	
+	<script type="text/javascript" src="/js/jquery.cycle.all.js"></script>
 		<script type="text/javascript">
 			$(document).ready(function() {
 				<?php if(!$ipad):?>
