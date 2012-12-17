27a78d4aadb4f05c2dce3b8d372461e2ac9e0849
Tanner Powell <thetannerpowell@gmail.com>
2012-12-17 13:39:11 -0600

    Crazyegg code to hana and austin PHP files
    "I need the following code added to your site before the closing
    </body> tag."

--- austin.php
+++ austin.php
@@ -333,5 +333,11 @@
                             });
         </script>
         <script type="text/javascript" src="/js/jquery.cycle.all.js"></script>
+        <script type="text/javascript">
+setTimeout(function(){var a=document.createElement("script");
+var b=document.getElementsByTagName("script")[0];
+a.src=document.location.protocol+"//dnn506yrbagrg.cloudfront.net/pages/scripts/0011/4602.js?"+Math.floor(new Date().getTime()/3600000);
+a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
+        </script>
     </body>
 </html>
