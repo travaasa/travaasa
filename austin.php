487458c49a1044c7a6d424e244563719b034afb7
Tanner Powell <thetannerpowell@gmail.com>
2013-02-23 15:15:56 -0600

    Austin Links test 001

--- austin.php
+++ austin.php
@@ -343,5 +343,6 @@ var b=document.getElementsByTagName("script")[0];
 a.src=document.location.protocol+"//dnn506yrbagrg.cloudfront.net/pages/scripts/0011/4602.js?"+Math.floor(new Date().getTime()/3600000);
 a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
         </script>
+        <script type="text/javascript">AppendNavisKeywordToLink("lnkBookingEngine1");</script>
     </body>
 </html>
